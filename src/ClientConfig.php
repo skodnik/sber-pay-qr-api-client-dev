176af11765b989bd5c60cbc64fdaaae531b02088
Evgeny Vlasov <vlasov.evgeny@gmail.com>
2022-09-02 16:29:19 +0200

    Рефакторинг

--- src/ClientConfig.php
+++ src/ClientConfig.php
@@ -6,30 +6,19 @@ namespace Vlsv\SberPayQrApiClient;
 
 class ClientConfig
 {
-    protected string $dateTimeFormat = 'Y-m-d\TH:i:s\Z';
-    protected string $host = 'https://api.sberbank.ru:8443/prod/qr/order/v3';
+    public function __construct(
+        protected string $host = 'https://api.sberbank.ru:8443/prod/qr/order/v3',
+        protected string $dateTimeFormat = 'Y-m-d\TH:i:s\Z',
+    ) {
+    }
 
     public function getDateTimeFormat(): string
     {
         return $this->dateTimeFormat;
     }
 
-    public function setDateTimeFormat(string $dateTimeFormat): ClientConfig
-    {
-        $this->dateTimeFormat = $dateTimeFormat;
-
-        return $this;
-    }
-
     public function getHost(): string
     {
         return $this->host;
-    }
-
-    public function setHost(string $host): ClientConfig
-    {
-        $this->host = $host;
-
-        return $this;
     }
 }
