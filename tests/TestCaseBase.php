abbf4d9e28c5067cb322a3d9c0c2ee6b927c955e
Evgeny Vlasov <vlasov.evgeny@gmail.com>
2022-09-02 13:19:29 +0200

    Вынес создание сериалайзера в фабрику

--- tests/TestCaseBase.php
+++ tests/TestCaseBase.php
@@ -4,23 +4,11 @@ declare(strict_types=1);
 
 namespace Vlsv\SberPayQrApiClient\Tests;
 
-use Doctrine\Common\Annotations\AnnotationReader;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
-use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
-use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
-use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
-use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
-use Symfony\Component\Serializer\NameConverter\MetadataAwareNameConverter;
-use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
-use Symfony\Component\Serializer\Normalizer\BackedEnumNormalizer;
-use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
-use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Vlsv\SberPayQrApiClient\ApiClient;
 use Vlsv\SberPayQrApiClient\ClientConfig;
+use Vlsv\SberPayQrApiClient\Factory\SerializerFactory;
 
 class TestCaseBase extends TestCase
 {
@@ -32,34 +20,7 @@ class TestCaseBase extends TestCase
     {
         parent::__construct($name, $data, $dataName);
 
-        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
-
-        $metadataAwareNameConverter = new MetadataAwareNameConverter(
-            $classMetadataFactory,
-            new CamelCaseToSnakeCaseNameConverter()
-        );
-
-        $normalizers = [
-            new BackedEnumNormalizer(),
-            new ArrayDenormalizer(),
-            new DateTimeNormalizer([
-                DateTimeNormalizer::FORMAT_KEY => 'Y-m-d\TH:i:s\Z',
-            ]),
-            new PropertyNormalizer(
-                classMetadataFactory: $classMetadataFactory,
-                nameConverter: $metadataAwareNameConverter,
-                propertyTypeExtractor: new PropertyInfoExtractor(
-                    typeExtractors: [
-                        new PhpDocExtractor(),
-                        new ReflectionExtractor(),
-                    ]
-                ),
-            ),
-
-        ];
-        $encoders = [new JsonEncoder()];
-
-        $this->serializer = new Serializer($normalizers, $encoders);
+        $this->serializer = SerializerFactory::getSerializer();
 
         $config = new ClientConfig();
         $this->apiClient = new ApiClient(
